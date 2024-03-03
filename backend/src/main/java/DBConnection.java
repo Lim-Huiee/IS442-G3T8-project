ae9627769bcbd920838cd950ab8cc67aa4365195
Jeremy Goh <jeremygmc_@hotmail.com>
2024-03-03 18:44:42 +0800

    started work on mysql db connection and ticket class
    db connection gets classnotfound error for java to mysql connector

---
+++ backend/src/main/java/DBConnection.java
@@ -0,0 +1,43 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnection {
+    private static String MYSQL_JDBC_DRIVER_CLASS = "com.mysql.cj.jdbc.Driver";
+    private static String MYSQL_DB_URL = "jdbc:mysql://localhost:3306/ticketmistress";
+    private static String MYSQL_DB_USER = "root";
+    private static String MYSQL_DB_USER_PASSWORD = "";
+    private static String SQL_QUERY = "Select * from user";
+
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName(MYSQL_JDBC_DRIVER_CLASS);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Failed to load MySQL JDBC driver. Error: " + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+            
+        try(Connection connection = DriverManager.getConnection(MYSQL_DB_URL,MYSQL_DB_USER,MYSQL_DB_USER_PASSWORD)) {
+
+            Class.forName(MYSQL_JDBC_DRIVER_CLASS); 
+            Statement statement =connection.createStatement();  
+            ResultSet resultSet = statement.executeQuery(SQL_QUERY); 
+
+            while(resultSet.next())  {
+                System.out.println(resultSet.getInt(1)+"  "+resultSet.getString(2));
+            }     
+                
+        } catch (ClassNotFoundException e) {
+            System.out.println("MySQL Driver class not found!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Error occured while executing query: " + SQL_QUERY);
+            e.printStackTrace();
+        } 
+    }
+}
