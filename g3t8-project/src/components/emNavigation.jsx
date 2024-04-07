7f87996e3ae8d1ba7995e9b9eb2f8dd1d34a86d3
limpes <pslim.2021@scis.smu.edu.sg>
2024-04-07 12:21:44 +0800

    handle logout

--- g3t8-project/src/components/emNavigation.jsx
+++ g3t8-project/src/components/emNavigation.jsx
@@ -10,6 +10,10 @@ export const EMNavigation = (props) => {
             navigate("/staff");
         }
     }, [])
+    const handleSignOut = () => {
+        localStorage.clear();
+        navigate("/staff");
+    };
 
     return (
         <nav id="menu" className="navbar navbar-default navbar-sticky-top navbar-expand-md my-0">
@@ -50,7 +54,7 @@ export const EMNavigation = (props) => {
                             <div className="dropdown-menu " aria-labelledby="navbarDropdown">
                                 {/* <a class="dropdown-item" href="#">Manage profile</a>
                                 <div class="dropdown-divider"></div> */}
-                                <a className="dropdown-item" href="#">Sign out</a>
+                                <a className="dropdown-item" href="#" onClick={handleSignOut}>Sign out</a>
                             </div>
                         </li>
                     </ul>
